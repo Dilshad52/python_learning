36794e432bed7c0161350f33207a9edf3e35b473
Dilshad52 <dilshadmohammed547@gmail.com>
2025-10-03 13:30:14 +0530

    Create variables-project.py
    Added variables program

---
+++ variables-project.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+age = input("How old are you? ") 
+hobby = input("What's your favorite hobby? ")
+print(f"Hey {name}! So you’re {age} years old and love {hobby}. That’s awesome!")
